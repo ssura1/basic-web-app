c81915a33745b1a1c4e6ca435172b8f703be041f
ssura1 <surastar208@gmail.com>
2026-02-23 15:34:36 +0000

    Added functionality to respond to AndrewID requests

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -7,6 +7,10 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
+  if ((query.toLowerCase().includes("andrew id")) || (query.toLowerCase().includes("andrewid"))) {
+    return "My Andrew ID is ssura";
+  }
+
   if (query.toLowerCase().includes("name")) {
     return "Rohan";
   }
